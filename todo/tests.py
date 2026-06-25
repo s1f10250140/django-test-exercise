f7aa5bcd4b6b86d0cfa736efa2f1a4009f367d4b
1F10250140 <s1f102501406@iniad.org>
2026-06-25 15:35:45 +0900

    詳細ページのテストを追加の修正

--- todo/tests.py
+++ todo/tests.py
@@ -102,7 +102,7 @@ class TodoViewTestCase(TestCase):
         response = client.get('/{}/'.format(task.pk))
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.template[0].name, 'todo/detail.html')
+        self.assertEqual(response.templates[0].name, 'todo/detail.html')
         self.assertEqual(response.context['task'], task)
 
     def test_detail_get_fail(self):
